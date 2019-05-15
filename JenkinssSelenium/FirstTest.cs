9079578918a18032a96ae1b346845f18095568af
SergenBAG <sergenbag01@gmail.com>
2019-05-15 23:11:17 +0300

    first commit

---
+++ JenkinssSelenium/FirstTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace JenkinssSelenium
+{
+    [TestFixture]
+    public class FirstTest
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.True(2 > 1);
+        }
+    }
+}
---
+++ JenkinssSelenium/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JenkinssSelenium")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JenkinssSelenium")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e4b546c0-19d9-4aac-b2e7-a477686e8bff")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
