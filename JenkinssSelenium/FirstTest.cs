c2271f861d60de4dade84fac27d24e4729c4691b
SergenBAG <sergenbag01@gmail.com>
2019-05-15 23:26:06 +0300

    added selenium

--- JenkinssSelenium/FirstTest.cs
+++ JenkinssSelenium/FirstTest.cs
@@ -1,4 +1,6 @@
 ﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
 
 namespace JenkinssSelenium
@@ -10,6 +12,14 @@ namespace JenkinssSelenium
         public void TestMethod1()
         {
             Assert.True(2 > 1);
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com");
+            driver.Quit();
         }
     }
 }
